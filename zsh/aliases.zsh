d1244e89841c3b1a58b7658ae45fafe54a273778
Devin Nakamura <devinn@ca.ibm.com>
2017-06-28 15:08:16 -0400

    Adjust grep alias
    Only add --color=auto if it is supported

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,14 @@
 # Color output (--color), add file type indication (-F),  print sizes in human redable form (-h)
 alias ls='ls -Fh --color'
 
-# Color grep output
-alias grep='grep --color=auto'
+grep-flag-available(){
+	echo | grep $1 "" > /dev/null 2>&1
+}
+GREP_FLAGS=""
+if grep-flag-available --color=auto; then
+	GREP_FLAGS+=" --color=auto"
+fi
 
+# Color grep output
+alias grep="grep $GREP_FLAGS"
+unset GREP_FLAGS
