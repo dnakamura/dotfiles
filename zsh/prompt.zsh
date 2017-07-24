a434e8f39a825750ba82988e64f1771d0bcb4a85
Devin Nakamura <devinn@ca.ibm.com>
2017-07-24 13:46:34 -0400

    Add user and host name to zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
 PROMPT_DIR_COLOR="%F{green}"
 
 #PROMPT="${PROMPT_DIR_COLOR}%d%f >"
-PROMPT='%F{$BASE0B}zsh%f %F{$BASE0D}%~%f%(!.#.)> '
+PROMPT='%F{$BASE0F}%n@%m %F{$BASE0B}zsh%f %F{$BASE0D}%~%f%(!.#.)> '
