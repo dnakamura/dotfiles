7d0b3b18c67d0d642014de6eb97c9bcc32e94fa2
Devin Nakamura <devinn@ca.ibm.com>
2018-05-15 13:21:34 -0400

    Revert to old colorscheme behaviour when dealing with putty

--- base16/init.sh
+++ base16/init.sh
@@ -2,6 +2,10 @@
 
 export BASE16_DIR="$(dirname $(readlink -f $0))"
 
+if [[ $TERM == putty* ]] ; then
+	BASE01_PROMPT=
+	$BASE16_DIR/set_theme.sh $theme_name
+fi
 set-colorscheme() {
 	theme_name=$1
 	if [ !  -f $BASE16_DIR/themes/$1 ]; then
@@ -14,6 +18,26 @@ set-colorscheme() {
 	fi
 
 	export BASE16_THEME=$theme_name
-	#$BASE16_DIR/set_theme.sh $theme_name
-	source "$BASE16_DIR/themes/$theme_name"
+	# putty doesnt like our new way of doing things
+	if [[ $TERM == putty* ]] ; then
+		$BASE16_DIR/set_theme.sh $theme_name
+		BASE00_PROMPT="%F{$BASE00}"
+		BASE01_PROMPT="%F{$BASE01}"
+		BASE02_PROMPT="%F{$BASE02}"
+		BASE03_PROMPT="%F{$BASE03}"
+		BASE04_PROMPT="%F{$BASE04}"
+		BASE05_PROMPT="%F{$BASE05}"
+		BASE06_PROMPT="%F{$BASE06}"
+		BASE07_PROMPT="%F{$BASE07}"
+		BASE08_PROMPT="%F{$BASE08}"
+		BASE09_PROMPT="%F{$BASE09}"
+		BASE0A_PROMPT="%F{$BASE0A}"
+		BASE0B_PROMPT="%F{$BASE0B}"
+		BASE0C_PROMPT="%F{$BASE0C}"
+		BASE0D_PROMPT="%F{$BASE0D}"
+		BASE0E_PROMPT="%F{$BASE0E}"
+		BASE0F_PROMPT="%F{$BASE0F}"
+	else
+		source "$BASE16_DIR/themes/$theme_name"
+	fi
 }
