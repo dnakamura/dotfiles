911a506e79722f55560d08edf96cf4374da7bd4a
Devin Nakamura <devinn@ca.ibm.com>
2018-06-12 14:07:40 -0400

    Change symlink for .zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,4 +2,4 @@
 ZSH_DOTFILES_DIR=$(dirname $(readlink -f -n $0))
 . "$ZSH_DOTFILES_DIR/../install_lib.sh"
 
-dotfiles_install "$ZSH_DOTFILES_DIR/zshrc" ~/.zshrc
+dotfiles_install "$ZSH_DOTFILES_DIR/.zshrc" ~/.zshrc
